29c2207015cb5220a9db7cab814e9ae34ad2720c
Namnguyen <72849@student.wsiz.edu.pl>
2026-04-01 00:52:44 +0200

    feat: Initialize Backend, connect to MongoDB Atlas, complete Fullstack Login/Register flow

---
+++ backend/models/User.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true, // Không cho phép 2 tài khoản trùng email
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true, // Tự động thêm ngày tạo (createdAt) và ngày cập nhật (updatedAt)
+  },
+);
+
+const User = mongoose.model("User", userSchema);
+export default User;
