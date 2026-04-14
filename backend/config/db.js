89289548761f43883aaa61781ee36e3dcebea195
Nam Nguyen <72849@student.wsiz.edu.pl>
2026-04-14 23:55:05 +0200

    feat(backend): Add Admin permissions, train management API, and Admin Seeder

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,9 +4,11 @@ const connectDB = async () => {
   try {
     // Kết nối tới MongoDB thông qua biến môi trường
     const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`🟢 MongoDB đã kết nối thành công: ${conn.connection.host}`);
+    console.log(
+      `🟢 MongoDB has successfully connected: ${conn.connection.host}`,
+    );
   } catch (error) {
-    console.error(`🔴 Lỗi kết nối MongoDB: ${error.message}`);
+    console.error(`🔴 MongoDB connection error: ${error.message}`);
     process.exit(1); // Dừng server nếu không kết nối được DB
   }
 };
